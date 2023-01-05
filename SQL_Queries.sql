ab948e4a8124b5b78b95db62f8f74f53b5938949
Mandy <momentarypause@gmail.com>
2023-01-05 14:54:20 -0500

    updated README, new queries, and new csv data file

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -10,3 +10,14 @@ FROM top_100_2022
 LEFT JOIN top_500_2019
 ON top_100_2022.name = top_500_2019.name
 ORDER BY top_100_2022.rank;
+
+
+-- Count of source categories
+SELECT source, COUNT(source) 
+FROM combined_net
+GROUP BY (source);
+
+-- Update source values to match distinct categories
+UPDATE combined_net
+SET source = 'Finance'
+WHERE source = 'Fintech' OR source = 'Bloomberg LP';
